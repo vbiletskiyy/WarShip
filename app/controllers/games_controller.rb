b73acf52e2ca246b2e4778f789d78cc2de808629
vbiletskiyy <98240473+vbiletskiyy@users.noreply.github.com>
2022-02-28 23:15:12 +0200

    Create game play (#5)
    * Create computer  board
    
    * create computer game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,8 +10,16 @@ class GamesController < ApplicationController
       @game = Game.create(user: current_user)
       @board = Board.create(user: current_user, game: @game)
       Cell.create_grid(@game.id, @board.id, current_user.id)
+      @enemy_board = Cell.create_grid(@game.id, @board.id, nil)
     end
     redirect_to board_url(@board), status: :found
   end
+ 
+  def show
+    @player_id = current_user.id
+    @user = current_user
+    @game=  Game.find(params["id"])
+    @cells = @game.cells
+    @comp_name = Faker::Name.name
+  end
 end
-
--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,13 +1,21 @@
 class Cell < ApplicationRecord
     belongs_to :game
     belongs_to :board
-    def self.create_grid(game_id, board_id, user_id)
+  def self.create_grid(game_id, board_id, user_id)
       a = [1,2,3,4,5] #number of rows To change gameboard size
       b = [1,2,3,4,5] #number of columns To change gameboard size
-      a.each do |n|
-        b.each do |m|
-          Cell.create({x:(m), y:(n), ship: false, game_id: game_id, board_id: board_id, player: user_id})
+        a.each do |n|
+          b.each do |m|
+            Cell.create({x:(m), y:(n), ship: false, game_id: game_id, board_id: board_id, player: user_id})
       end
+    end
+  end
+
+  def self.pick_random
+    7.times() do 
+      d = @enemy_board.cells.where(player: nil, ship: false).order("RANDOM()").first
+      d.ship = true
+      d.save
     end
   end
 end
