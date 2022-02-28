139b905901647158d08c9955aec560780b023fb6
Bodyk1Love <vanya.zaliznyak@gmail.com>
2022-02-28 23:12:05 +0200

    fix enemy ship create

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,7 +1,15 @@
 class Board < ApplicationRecord
-    has_many :ships
-    has_many :shots
-    belongs_to :user
-    belongs_to :game
-    has_many :cells
+  has_many :ships
+  has_many :shots
+  belongs_to :user
+  belongs_to :game
+  has_many :cells
+
+  def pick_random
+    cells
+      .where(ship: false)
+      .order("RANDOM()")
+      .limit(7)
+      .update_all(ship: true)
+  end
 end
--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,21 +1,12 @@
 class Cell < ApplicationRecord
-    belongs_to :game
-    belongs_to :board
-  def self.create_grid(game_id, board_id, user_id)
-      a = [1,2,3,4,5] #number of rows To change gameboard size
-      b = [1,2,3,4,5] #number of columns To change gameboard size
-        a.each do |n|
-          b.each do |m|
-            Cell.create({x:(m), y:(n), ship: false, game_id: game_id, board_id: board_id, player: user_id})
-      end
-    end
-  end
+  belongs_to :game
+  belongs_to :board
 
-  def self.pick_random
-    7.times() do 
-      d = @enemy_board.cells.where(player: nil, ship: false).order("RANDOM()").first
-      d.ship = true
-      d.save
+  def self.create_grid(game_id, board_id, user_id)
+    (1..5).each do |n|
+      (1..5).each do |m|
+        Cell.create(x: m, y: n, ship: false, game_id: game_id, board_id: board_id, player: user_id)
+      end
     end
   end
 end
